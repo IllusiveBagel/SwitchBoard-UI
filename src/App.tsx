cc4056c043b5cec9ffdedf9977732971899bfbda
IllusiveBagel <logan@neutral.systems>
2024-09-25 16:15:05 +0100

    Basic test of getting data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { useState, useEffect } from "react";
+import { Switch } from "./interfaces/Switch";
+
+const baseURL = import.meta.env.VITE_BASE_URL;
 
 const App = () => {
-  const [switches, setSwitches] = useEffect();
+  const [switches, setSwitches] = useState([]);
+
+  useEffect(() => {
+    fetch(`${baseURL}/switches`)
+      .then((response) => response.json())
+      .then((json) => setSwitches(json))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <>
       <h1>SwitchBoard</h1>
+      {switches.map((s: Switch) => {
+        return (
+          <li>{s.name}</li>
+        );
+      })}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
