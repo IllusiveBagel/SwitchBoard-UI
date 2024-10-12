7d197d28e47989300b59daa332462f81b8190376
IllusiveBagel <logan@neutral.systems>
2024-10-12 16:44:58 +0100

    Added Error page

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./Routes/Root.tsx";
+import ErrorPage from "./ErrorPage.tsx";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
