a9dff70e1e0bd76d3f06455c43825675cedb540d
IllusiveBagel <logan@neutral.systems>
2024-11-17 00:34:00 +0000

    started to flesh out tailwind inplementation

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
     return (
         <>
             <Navigation />
-            <div className="content">
+            <div className="fixed bottom-0 right-0 h-[calc(100vh-4rem)] w-[calc(100vw-15rem)] overflow-y-auto">
                 <Outlet />
             </div>
         </>
--- src/Components/Navigation/index.tsx
+++ src/Components/Navigation/index.tsx
@@ -1,23 +1,40 @@
 import { Link } from "react-router-dom";
+import {
+    HomeIcon,
+    Squares2X2Icon,
+    InformationCircleIcon,
+} from "@heroicons/react/24/outline";
 
 const Navigation = () => {
     return (
         <>
             <div className="wlvw h-16 bg-slate-800"></div>
-            <div className="w-80 h-[calc(100vh-4rem)] bg-slate-700">
-                <ul>
+            <div className="w-60 h-[calc(100vh-4rem)] bg-slate-700">
+                <ul className="list-none">
                     <li>
-                        <Link className="" to="/">
+                        <Link
+                            className="text-2xl w-full flex flex-row items-center gap-3 pt-5 pl-3.5"
+                            to="/"
+                        >
+                            <HomeIcon className="size-7" />
                             Home
                         </Link>
                     </li>
                     <li>
-                        <Link className="" to="/switches">
+                        <Link
+                            className="text-2xl w-full flex flex-row items-center gap-3 pt-5 pl-3.5"
+                            to="/switches"
+                        >
+                            <Squares2X2Icon className="size-7" />
                             Switches
                         </Link>
                     </li>
                     <li>
-                        <Link className="" to="/about">
+                        <Link
+                            className="text-2xl w-full flex flex-row items-center gap-3 pt-5 pl-3.5"
+                            to="/about"
+                        >
+                            <InformationCircleIcon className="size-7" />
                             About
                         </Link>
                     </li>
