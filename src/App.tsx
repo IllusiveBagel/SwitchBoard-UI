7ef122b887014a7496a320d4a0e92ab4ff39a484
IllusiveBagel <logan@neutral.systems>
2024-11-17 21:44:25 +0000

    Added to table layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
     return (
         <>
             <Navigation />
-            <div className="fixed bottom-0 right-0 h-[calc(100vh-4rem)] w-[calc(100vw-15rem)] overflow-y-auto bg-gray-900">
+            <div className="fixed bottom-0 right-0 h-[calc(100vh-4rem)] w-[calc(100vw-15rem)] overflow-y-auto bg-gray-900 block">
                 <Outlet />
             </div>
         </>
--- src/Routes/Switches.tsx
+++ src/Routes/Switches.tsx
@@ -31,14 +31,13 @@ const Switches = () => {
         );
     return (
         <>
-            <div className="flex w-full h-12 items-center justify-end px-4 bg-transparent mt-5 gap-3 fixed top-12 left-0">
-                {!isTable && (
+            <div className="flex w-[calc(100vw-15rem)] h-16 items-center justify-end px-4 bg-transparent gap-3 fixed top-16 right-0">
+                {!isTable ? (
                     <TableCellsIcon
                         className="size-10"
                         onClick={() => setIsTable(true)}
                     />
-                )}
-                {isTable && (
+                ) : (
                     <Squares2X2Icon
                         className="size-10"
                         onClick={() => setIsTable(false)}
@@ -47,35 +46,87 @@ const Switches = () => {
                 <FunnelIcon className="size-10" />
             </div>
             {!isTable && (
-                <div className="grid grid-cols-8 gap-4 m-5 mt-[3.75rem]">
+                <div className="grid grid-cols-10 gap-4 m-5 mt-16">
                     {switches?.map((item) => (
                         <GridItem key={item.id} name={item.name} />
                     ))}
                 </div>
             )}
             {isTable && (
-                <table className="table-auto border-collapse border border-slate-500 m-5 mt-[3.75rem]">
-                    <thead>
-                        <tr>
-                            <th className="border border-slate-600">Name</th>
-                            <th className="border border-slate-600">
-                                Manufacturer
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {switches?.map((item) => (
+                <div className="w-[calc(100%-2.5rem)] m-5 mt-16">
+                    <table className="border-collapse border border-slate-500 w-full">
+                        <thead>
                             <tr>
-                                <td className="border border-slate-700">
-                                    {item.name}
-                                </td>
-                                <td className="border border-slate-700">
-                                    {item.manufacturer}
-                                </td>
+                                <th className="border border-slate-600 bg-slate-700">
+                                    Name
+                                </th>
+                                <th className="border border-slate-600 bg-slate-700">
+                                    Manufacturer
+                                </th>
+                                <th className="border border-slate-600 bg-slate-700">
+                                    Actuation
+                                </th>
+                                <th className="border border-slate-600 bg-slate-700">
+                                    Bottom-Out
+                                </th>
+                                <th className="border border-slate-600 bg-slate-700">
+                                    Pre-Travel
+                                </th>
+                                <th className="border border-slate-600 bg-slate-700">
+                                    Total-Travel
+                                </th>
+                                <th className="border border-slate-600 bg-slate-700">
+                                    Type
+                                </th>
+                                <th className="border border-slate-600 bg-slate-700">
+                                    Spring
+                                </th>
+                                <th className="border border-slate-600 bg-slate-700">
+                                    Volume
+                                </th>
+                                <th className="border border-slate-600 bg-slate-700">
+                                    Factory Lubed
+                                </th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody className="text-right">
+                            {switches?.map((item) => (
+                                <tr>
+                                    <td className="border border-slate-700 bg-slate-800 text-left">
+                                        {item.name}
+                                    </td>
+                                    <td className="border border-slate-700 bg-slate-800">
+                                        {item.manufacturer}
+                                    </td>
+                                    <td className="border border-slate-700 bg-slate-800">
+                                        {item.actuation}
+                                    </td>
+                                    <td className="border border-slate-700 bg-slate-800">
+                                        {item.bottomOut}
+                                    </td>
+                                    <td className="border border-slate-700 bg-slate-800">
+                                        {item.preTravel}
+                                    </td>
+                                    <td className="border border-slate-700 bg-slate-800">
+                                        {item.totalTravel}
+                                    </td>
+                                    <td className="border border-slate-700 bg-slate-800">
+                                        {item.type}
+                                    </td>
+                                    <td className="border border-slate-700 bg-slate-800">
+                                        {item.spring}
+                                    </td>
+                                    <td className="border border-slate-700 bg-slate-800">
+                                        {item.volume}
+                                    </td>
+                                    <td className="border border-slate-700 bg-slate-800">
+                                        {item.factoryLubed}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             )}
         </>
     );
